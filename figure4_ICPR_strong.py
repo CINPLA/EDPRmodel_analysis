52cab0d2b3856b0dea238d9e85068095d6d9235c
Marte Julie <m.j.satra@fys.uio.no>
2019-12-22 15:39:39 +0100

    figure 4 added

---
+++ figure4_ICPR_strong.py
@@ -0,0 +1,48 @@
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+from ICPRmodel.ICPRmodel import *
+from functions.solve_ICPRmodel import *
+from functions.print_final_values import *
+from functions.calculate_sigma import *
+from functions.calculate_ATP import *
+
+start_time = time.time()
+
+t_dur = 30      # [s]
+alpha = 2   
+I_stim = 28e-12 # [A]
+stim_start = 10 # [s]
+stim_end = 20   # [s]
+
+sol = solve_ICPRmodel(t_dur, alpha, I_stim, stim_start, stim_end)
+
+Na_si, Na_se, Na_di, Na_de, K_si, K_se, K_di, K_de, Cl_si, Cl_se, Cl_di, Cl_de, Ca_si, Ca_se, Ca_di, Ca_de, \
+    k_res_si, k_res_se, k_res_di, k_res_de, n, h, s, c, q, z = sol.y
+t = sol.t
+
+my_cell = ICPRmodel(309.14, Na_si, Na_se, Na_di, Na_de, K_si, K_se, K_di, K_de, Cl_si, Cl_se, Cl_di, Cl_de, Ca_si, Ca_se, Ca_di, Ca_de, k_res_si, k_res_se, k_res_di, k_res_de, alpha, Ca_si[0], Ca_di[0], n, h, s, c, q, z)
+print_final_values(my_cell)
+
+phi_si, phi_se, phi_di, phi_de, phi_sm, phi_dm = my_cell.membrane_potentials()
+
+E_Na_s, E_Na_d, E_K_s, E_K_d, E_Cl_s, E_Cl_d, E_Ca_s, E_Ca_d = my_cell.reversal_potentials()
+
+print("----------------------------")
+print('elapsed time: ', round(time.time() - start_time, 1), 'seconds')
+
+f1 = plt.figure(1)
+plt.plot(t, phi_sm*1000, '-', label='V_s')
+plt.plot(t, phi_dm*1000, '-', label='V_d')
+plt.title('Membrane potentials')
+plt.xlabel('time [s]')
+plt.ylabel('[mV]')
+plt.legend(loc='upper right')
+
+# save to file
+np.savez('data/figure4_ICPR_strong', t=t, phi_si=phi_si, phi_se=phi_se, phi_di=phi_di, phi_de=phi_de, phi_sm=phi_sm, phi_dm=phi_dm, \
+    E_Na_s=E_Na_s, E_Na_d=E_Na_d, E_K_s=E_K_s, E_K_d=E_K_d, E_Cl_s=E_Cl_s, E_Cl_d=E_Cl_d, E_Ca_s=E_Ca_s, E_Ca_d=E_Ca_d, \
+    Na_si=Na_si, Na_se=Na_se, Na_di=Na_di, Na_de=Na_de, K_si=K_si, K_se=K_se, K_di=K_di, K_de=K_de, Cl_si=Cl_si, Cl_se=Cl_se, Cl_di=Cl_di, Cl_de=Cl_de, \
+    Ca_si=Ca_si, Ca_se=Ca_se, Ca_di=Ca_di, Ca_de=Ca_de, free_Ca_di=my_cell.free_Ca_di)
+
+plt.show()
---
+++ figure4_PR_weak.py
@@ -0,0 +1,33 @@
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+from originalPRmodel import solve_originalPRmodel
+
+start_time = time.time()
+
+t_dur = 30e3      # [ms]
+g_c = 2.26        # [mS/cm^2] Weak
+I_stim = 1.35     # [uA/cm^2]
+stim_start = 10e3 # [ms]
+stim_end = 20e3   # [ms]
+
+sol = solve_originalPRmodel(t_dur, g_c, I_stim, stim_start, stim_end)
+
+Vs, Vd, n, h, s, c, q, Ca = sol.y
+t = sol.t
+
+print('elapsed time: ', round(time.time() - start_time, 1), 'seconds')
+
+# plot
+f1 = plt.figure(1)
+plt.plot(t/1000, Vs, '-', label='V_s')
+plt.plot(t/1000, Vd, '-', label='V_d')
+plt.title('Membrane potentials')
+plt.xlabel('time [s]')
+plt.ylabel('[mV]')
+plt.legend(loc='upper right')
+
+# save to file
+np.savez('data/figure4_PR_weak', t=t, Vs=Vs, Vd=Vd, Ca=Ca)
+
+plt.show()
