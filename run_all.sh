230c832752f3047dd6e677ba2381924c88d73f9e
Marte Julie <m.j.satra@fys.uio.no>
2020-01-08 15:25:42 +0100

    script to run all code added

---
+++ run_all.sh
@@ -0,0 +1,32 @@
+echo "Create data for Figure 3..."
+python3 figure3.py
+echo "Figure 3 done."
+
+echo "Create data for Figure 4..."
+python3 figure4_PR_weak.py
+python3 figure4_PR_strong.py
+python3 figure4_ICPR_weak.py
+python3 figure4_ICPR_strong.py
+echo "Figure 4 done."
+
+echo "Create data for Figure 5..."
+python3 figure5_PR.py
+python3 figure5_ICPR.py
+echo "Figure 5 done."
+
+echo "Create data for Figure 6..."
+python3 figure6_PR.py
+python3 figure6_ICPR.py
+echo "Figure 5 done."
+
+echo "Create data for Figure 7..."
+python3 figure7.py
+echo "Figure 7 done."
+
+echo "Create data for Figure 8..."
+python3 figure8.py
+echo "Figure 8 done."
+
+echo "Create data for Figure 9..."
+python3 figure9.py
+echo "Figure 9 done."
