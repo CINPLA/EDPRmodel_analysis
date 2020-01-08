594dadc046f191ca469c74c7b6621771bfe1f362
Marte Julie <m.j.satra@fys.uio.no>
2020-01-08 15:27:55 +0100

    script to plot all figures added

---
+++ figures/plot_all.sh
@@ -0,0 +1,9 @@
+echo "Plot all figures..."
+python plot_figure3.py
+python plot_figure4.py
+python plot_figure5.py
+python plot_figure6.py
+python plot_figure7.py
+python plot_figure8.py
+python plot_figure9.py
+echo "Figures done."
