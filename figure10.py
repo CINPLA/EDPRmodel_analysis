68d0a8d0df328f80e491b3ce9c117fb2ef992d03
Marte Julie <m.j.satra@fys.uio.no>
2020-03-02 13:03:31 +0100

    I_stim updated

--- figure10.py
+++ figure10.py
@@ -10,7 +10,7 @@ start_time = time.time()
 
 t_dur = 30      # [s]
 alpha = 2.0
-I_stim = 28e-12 # [A]
+I_stim = 27e-12 # [A]
 stim_start = 10 # [s]
 stim_end = 20   # [s]
 
@@ -20,7 +20,7 @@ Na_si, Na_se, Na_di, Na_de, K_si, K_se, K_di, K_de, Cl_si, Cl_se, Cl_di, Cl_de, 
     k_res_si, k_res_se, k_res_di, k_res_de, n, h, s, c, q, z = sol.y
 t = sol.t
 
-my_cell = EDPRmodel(309.14, Na_si, Na_se, Na_di, Na_de, K_si, K_se, K_di, K_de, Cl_si, Cl_se, Cl_di, Cl_de, Ca_si, Ca_se, Ca_di, Ca_de, k_res_si, k_res_se, k_res_di, k_res_de, alpha, Ca_si[0], Ca_di[0], n, h, s, c, q, z)
+my_cell = EDPRmodel(309.14, Na_si, Na_se, Na_di, Na_de, K_si, K_se, K_di, K_de, Cl_si, Cl_se, Cl_di, Cl_de, Ca_si, Ca_se, Ca_di, Ca_de, k_res_si, k_res_se, k_res_di, k_res_de, alpha, 0.01, 0.01, n, h, s, c, q, z)
 #print_final_values(my_cell)
 
 phi_si, phi_se, phi_di, phi_de, phi_sm, phi_dm = my_cell.membrane_potentials()
