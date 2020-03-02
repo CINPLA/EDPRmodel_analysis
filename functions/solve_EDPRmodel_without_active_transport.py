178b0eb5cf67fc002ad1aacbdb6eaac276d3a074
Marte Julie <m.j.satra@fys.uio.no>
2020-03-02 11:34:44 +0100

    typo fixed

--- functions/solve_EDPRmodel_without_active_transport.py
+++ functions/solve_EDPRmodel_without_active_transport.py
@@ -79,6 +79,6 @@ def solve_EDPRmodel_without_active_transport(t_dur, alpha):
     k0 = [Na_si0, Na_se0, Na_di0, Na_de0, K_si0, K_se0, K_di0, K_de0, Cl_si0, Cl_se0, Cl_di0, Cl_de0, Ca_si0, Ca_se0, Ca_di0, Ca_de0, \
         k_res_si0, k_res_se0, k_res_di0, k_res_de0, n0, h0, s0, c0, q0, z0]
 
-    sol = solve_ivp(calibrate, t_span, k0, max_step=1e-4)
+    sol = solve_ivp(dkdt, t_span, k0, max_step=1e-4)
 
     return sol
