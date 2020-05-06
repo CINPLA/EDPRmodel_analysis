1a2194400614999a48cdb43bd521e0c4b0e4e9fb
Marte Julie <m.j.satra@fys.uio.no>
2020-05-06 11:11:54 +0200

    corrected parameter name in calculate_ATP

--- functions/calculate_ATP.py
+++ functions/calculate_ATP.py
@@ -13,8 +13,8 @@ def calculate_ATP(my_cell, t):
     ATP_pump_per_sec = (j_pump_s * my_cell.A_s + j_pump_d * my_cell.A_d) * N_A
     ATP_pump = integrate.cumtrapz(ATP_pump_per_sec, t, initial=0)
 
-    j_Ca_dec_s = my_cell.tau * (my_cell.Ca_si - my_cell.Ca0_si)*my_cell.V_si/my_cell.A_s
-    j_Ca_dec_d = my_cell.tau * (my_cell.Ca_di - my_cell.Ca0_di)*my_cell.V_di/my_cell.A_d
+    j_Ca_dec_s = my_cell.U_Cadec * (my_cell.Ca_si - my_cell.Ca0_si)*my_cell.V_si/my_cell.A_s
+    j_Ca_dec_d = my_cell.U_Cadec * (my_cell.Ca_di - my_cell.Ca0_di)*my_cell.V_di/my_cell.A_d
     
     ATP_Ca_per_sec = (j_Ca_dec_s * my_cell.A_s + j_Ca_dec_d * my_cell.A_d) * N_A
     ATP_Ca = integrate.cumtrapz(ATP_Ca_per_sec, t, initial=0)
